906addccea39e2453530fd6ad0c26cf78e9eecd6
BranCorrea <branjibran96@gmail.com>
2024-09-04 00:04:10 -0500

    tareasuma

---
+++ tareasuma/tareasuma.js
@@ -0,0 +1,13 @@
+let numero1 = parseFloat(prompt("ingresa el numero 1"));
+let numero2 = parseFloat(prompt("ingresa el numero 2"));
+
+
+function suma(numero1, numero2){
+
+let suma= numero1 + numero2;
+
+alert("la suma es: "+suma);
+return suma;
+
+}
+suma(numero1, numero2);

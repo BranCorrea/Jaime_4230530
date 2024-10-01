cf0db8083c1b6fa7fd0a79f4049e1660e5e75f88
BranCorrea <branjibran96@gmail.com>
2024-10-01 10:20:03 -0500

    tarea4

---
+++ tarea4/tareas.js
@@ -0,0 +1,43 @@
+let frtutas = ["manzana", "pera", "sandia", "naranja"];
+frtutas.forEach(function(valor, indice){
+        console.log(indice + ": " + valor)
+});
+
+let cadena = "";
+for(i = 0; i<=3; i++){
+    cadena = cadena + "X";}
+    console.log(cadena)
+
+    let cadena1 = "";
+    for(i = 0; i<=3; i++){
+        cadena1 = cadena1 + "X\n";}
+        console.log(cadena)
+
+let a = parseInt(prompt("cuantas veces deseas imprimir"))
+
+function imprimir(a){
+    let cadena = "";
+for(i = 0; i<= a; i++){
+    cadena = cadena + "X";}
+    console.log(cadena)
+}
+
+function imprimir(a){
+    let cadena = "";
+for(i = 0; i<= a; i++){
+    cadena = cadena + "X\n";}
+    console.log(cadena)
+}
+
+function tablas(){
+    for(a=1; a<=10; a++){
+        multiplicacion = a * b ;
+        console.log(b+"x"+a+"="+multiplicacion+"\n \n")
+
+    }
+}
+
+for(b=1; b<=10; b++){
+    tablas(b);
+}
+
